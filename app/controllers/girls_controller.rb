942aa8f3d9ccdefc99c585ad34929d406027f599
Che Chaoyang <chaoyang.che@dji.com>
2015-08-13 13:32:08 +0800

    修改学员报名表格导入功能，报名表只保留姓名和邮箱

--- app/controllers/girls_controller.rb
+++ app/controllers/girls_controller.rb
@@ -35,18 +35,18 @@ class GirlsController < ApplicationController
   def import
     require 'csv'
 
-    # csv format: "微博昵称,粉丝数,微博数,真实姓名,联系地址,联系电话,电子邮箱"
-    # csv origin encoding: GBK
+    # csv format: "姓名,邮箱"
+    # csv origin encoding: UTF-8
     # csv separator: comma
-    CSV.open(params[:csv].tempfile, 'r:GBK:UTF-8') do |csv|
+    CSV.open(params[:csv].tempfile) do |csv|
       header = csv.shift
-      unless header[0] == '微博昵称' and header[6] == '电子邮箱'
+      unless header[0] == 'Name' and header[1] == 'Email'
         return redirect_to event_girls_path(params[:event_id]),
                            :notice => t('event.import.fail', :filename => params[:csv].original_filename)
       end
 
       csv.each do |row|
-        current_event.girls.create :name => row[0], :email => row[6], :mobile => row[5]
+        current_event.girls.create :name => row[0], :email => row[1]
       end
 
       redirect_to event_girls_path(params[:event_id]),
